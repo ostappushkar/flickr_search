df29adbb3b4da6194db0fbbbd6152b0ec7f180cd
Ostap Pushkar <pushkarostap@gmail.com>
2019-11-08 14:57:27 +0200

    App without unit-tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Flickr from'flickr-sdk';
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      searchString:"",
+      photoItems:[]
+    }
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-function App() {
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    var flickr = new Flickr(process.env.REACT_APP_FLICKR_API_KEY);
+    flickr.photos.search({text:this.state.searchString}).then((res)=>{
+      this.setState({photoItems:res.body.photos.photo})
+    })
+  }
+  handleTextChange(event){
+    this.setState({ searchString: event.target.value });
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+          <form onSubmit={this.handleSubmit}>
+            <input type="search"       value={this.state.searchString} onChange={this.handleTextChange} name="searchText"></input>
+            <input type="submit"  value="Search"></input>
+          </form>
+        </header>
+        <section>
+          <div className="photosContainer"> {this.state.photoItems.map((item,key)=>{
+            return(
+              <div id={key} className="photoItem">
+                <img loading="lazy" alt="" src={ `http://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`}></img>
+                <p>{item.title}</p>
+              </div>
+            )
+          })}</div>
+         
+
+        </section>
     </div>
   );
+}
 }
 
 export default App;
